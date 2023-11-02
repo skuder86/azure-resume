0bb379572a8893527db003acbd3d8dac53f7fa4b
skuder86 <s.skumbim@me.com>
2023-11-02 11:52:01 +0100

    Created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApiUrl = 'https://getvisitorcounter.azurewebsites.net/api/GetVisitorCounter';
+
 const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
